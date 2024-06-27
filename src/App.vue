4e3672dfea5c3ed17e45fabb466a0a9b274b9226
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-06-27 17:28:23 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,41 @@ import { ref } from 'vue';
 
 const isHovered = ref(false);
 
-const toggleHover = () => {
-  isHovered.value = !isHovered.value;
+const handleMouseOver = () => {
+  isHovered.value = true;
+};
+
+const handleMouseLeave = () => {
+  isHovered.value = false;
 };
 </script>
 
 <template>
   <div
-      :class="['flex w-screen h-screen justify-center items-center', !isHovered ? 'bg-white' : 'bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] ']"
-
+      class="flex w-screen h-screen justify-center items-center transition-colors duration-300"
+      :class="[isHovered ? 'bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800]' : 'bg-white']"
   >
     <div
-        :class="['group px-8 py-4 select-none']"
-        @mouseover="toggleHover"
-        @mouseleave="toggleHover"
+        class="group px-8 py-4 select-none"
+        @mouseover="handleMouseOver"
+        @mouseleave="handleMouseLeave"
     >
-      <h1 :class="[!isHovered ? 'block' : 'hidden', 'text-9xl text-transparent bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] inline-block bg-clip-text ']">IsoMaker *</h1>
-      <h1 :class="[!isHovered ? 'hidden' : 'block', 'text-9xl text-white']">Make Your World</h1>
-
+      <h1
+          :class="[
+          'text-9xl text-transparent bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] inline-block bg-clip-text transition-opacity duration-300',
+          isHovered ? 'hidden' : 'block'
+        ]"
+      >
+        IsoMaker *
+      </h1>
+      <h1
+          :class="[
+          'text-9xl text-white transition-opacity duration-300',
+          isHovered ? 'block' : 'hidden'
+        ]"
+      >
+        Make Your World
+      </h1>
     </div>
   </div>
 </template>
