07806f9b5bd2ea931aad8153fb3c5fd4991aa630
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-06-27 17:23:15 +0200

    feat: base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const isHovered = ref(false);
+
+const toggleHover = () => {
+  isHovered.value = !isHovered.value;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+      :class="['flex w-screen h-screen justify-center items-center', isHovered ? 'bg-white' : 'bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] ']"
+
+  >
+    <div
+        :class="['group px-8 py-4 select-none']"
+        @mouseover="toggleHover"
+        @mouseleave="toggleHover"
+    >
+      <h1 :class="[isHovered ? 'block' : 'hidden', 'text-9xl text-transparent bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] inline-block bg-clip-text ']">IsoMaker *</h1>
+      <h1 :class="[isHovered ? 'hidden' : 'block', 'text-9xl text-white']">Make Your World</h1>
+
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
