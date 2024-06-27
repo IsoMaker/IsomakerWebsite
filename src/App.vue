0eb2a95b35ba18f347b978b24a88b35ccd2e3402
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-06-27 17:25:29 +0200

    fix: order

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const toggleHover = () => {
 
 <template>
   <div
-      :class="['flex w-screen h-screen justify-center items-center', isHovered ? 'bg-white' : 'bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] ']"
+      :class="['flex w-screen h-screen justify-center items-center', !isHovered ? 'bg-white' : 'bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] ']"
 
   >
     <div
@@ -18,8 +18,8 @@ const toggleHover = () => {
         @mouseover="toggleHover"
         @mouseleave="toggleHover"
     >
-      <h1 :class="[isHovered ? 'block' : 'hidden', 'text-9xl text-transparent bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] inline-block bg-clip-text ']">IsoMaker *</h1>
-      <h1 :class="[isHovered ? 'hidden' : 'block', 'text-9xl text-white']">Make Your World</h1>
+      <h1 :class="[!isHovered ? 'block' : 'hidden', 'text-9xl text-transparent bg-gradient-to-r from-[#FF218C] to-[#21B1FF] via-[#FFD800] inline-block bg-clip-text ']">IsoMaker *</h1>
+      <h1 :class="[!isHovered ? 'hidden' : 'block', 'text-9xl text-white']">Make Your World</h1>
 
     </div>
   </div>
